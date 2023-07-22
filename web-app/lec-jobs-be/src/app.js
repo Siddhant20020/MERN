1e32b9e6b010cb3d12012b9c22f2b097a5285a80
Siddhant20020 <raiprachu1@gmail.com>
2023-07-22 07:27:42 +0545

    BE

--- web-app/lec-jobs-be/src/app.js
+++ web-app/lec-jobs-be/src/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const fs=require("fs");
+const cors =require("cors");
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
+app.use(cors());
 // http://loacal host:500/ or  https://loacal host:500/ 
+
 
 app.get("/", 
 (req, res) => 
 { res.status(200).send("This is response from BE"); }
-)
+);
+//read file and send content of file as response
+app.get("/api/v1/posts", 
+(req, res) => 
+{ const posts=fs.readFileSync("./data/posts.json","utf-8").toString();
+    res.status(200).send(posts); }
+);
 app.listen(PORT, () => {
     console.log("App is running on port" + PORT);
-
 });
